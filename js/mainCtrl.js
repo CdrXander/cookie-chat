c89ea5d1b64fb3fc29eafd198180d1b13c61017f
CdrXander <xackhackman@gmail.com>
2016-11-16 15:16:10 -0700

    Service pretty much working. Trying to figure out cookies.

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -3,20 +3,59 @@ angular.module('chatroom').controller('mainCtrl', function($scope, messageServic
   //The getMessages function will call the getData method on the messageService object.
   //You'll then save the result of that request to your controller's $scope as messages ($scope.messages)
 
+  $scope.getMessages = function() {
+    messageService.getMessages().then(function(response) {
+      $scope.messages = response.data;
+    });
+  }
 
-
-
-  //The postMessage function will take whatever the user typed in (hint: look at the html and see what ng-model correlates to on the input box), 
+  //The postMessage function will take whatever the user typed in 
+  //(hint: look at the html and see what ng-model correlates to on the input box), 
   //pass that text to the postMessage method on the messageService object which will
   //then post it to the backend.
 
+  $scope.postMessage = function() {
+    messageService.postMessage($scope.message).then(function(response) {
+        $scope.getMessages();
+        $scope.message = '';
+    });
+  }
 
+
+  //Das Cookie Methods
+  $scope.getCookies = function() {
+    messageService.getCookies().then(function(response){
+      $scope.cookies = response.data.cookies;
+    })
+  }
+
+  $scope.postCookie = function() {
+      messageService.postCookie($scope.cookie).then(function(response) {
+        $scope.getCookies();
+        $scope.newCookie = '';
+    });
+  }
+
+  $scope.getMessages();
+  $scope.getCookies();
 
   //uncomment this code when your getMessages function is finished
   //This goes and gets new data every second, which mimicking a chat room experience.
-  //  setInterval(function(){
-    // $scope.getMessages();
-
-  // }, 1500)
+  setInterval(function(){
+    $scope.getMessages();
+    $scope.getCookies();
+  }, 1500)
 
 })
+
+/*{"cookies":
+  {
+    "Tasty Chocolate Chip Cookie":4,
+    "Delicious Butter Pecan Cookie":3,
+    "Delectable Fortune Cookie":27,
+    "Delicious Macaroon Cookie":4,
+    "Delectable Chocolate Chip Cookie":3,
+    "Flavorful Butter Pecan Cookie":6,
+    "Tasty Macaroon Cookie":34
+  }
+}*/
--- js/messageService.js
+++ js/messageService.js
@@ -5,15 +5,54 @@ angular.module('chatroom').service('messageService', function($http){
   //The url for the get request should be 'http://practiceapi.devmounta.in/api/chats'
   //Be sure to return whatever gets returned from $http so you can call .then in your controller.
 
-
+  this.getMessages = function() {
+  	return $http({
+  		method: 'GET',
+  		url: 'http://practiceapi.devmounta.in/api/chats'
+  	});
+  };
 
 
   //On the line below create the postMessage method. This method will add data to the backend server.
   //The url for the request needs to be 'http://practiceapi.devmounta.in/api/chats'
-  //Because we're making a POST request, we need a way to tell the server the data we want to give it, in your $http call (along with url and method) have a data property which has a value that is equal to another object with a key of message and a value of the message being passed to parse. IE data: {message: yourMessage}
+  //Because we're making a POST request, we need a way to tell the server the data we want to give it, 
+  //in your $http call (along with url and method) have a data property which has a value that is equal 
+  ///to another object with a key of message and a value of the message being passed to parse. IE data: {message: yourMessage}
   //Also, remember that $http returns a promise. So if you return the whole $http call (return $http(...)), you can then use .then in your controller.
 
+  this.postMessage = function(newMsg) {
+  	return $http({
+  		method: 'POST',
+  		url: 'http://practiceapi.devmounta.in/api/chats',
+  		data: {message: newMsg}
+  	})
+  };
+
+  //Das cookie methods
+  this.getCookies = function() {
+  	return $http({
+  		method:'GET',
+  		url: 'http://practiceapi.devmounta.in/api/cookies'
+  	})
+  }
 
 
+  this.postCookie = function(newCookie) {
+  	return $http({
+  		method: 'POST',
+  		url: 'http://practiceapi.devmounta.in/api/cookies',
+  		data: {"cookies":
+  				{
+				    "Tasty Chocolate Chip Cookie":1,
+				    "Delicious Butter Pecan Cookie":1,
+				    "Delectable Fortune Cookie":1,
+				    "Delicious Macaroon Cookie":1,
+				    "Delectable Chocolate Chip Cookie":1,
+				    "Flavorful Butter Pecan Cookie":1,
+    				"Tasty Macaroon Cookie":1
+  				}
+			}
+  	})
+  }
 
 });
